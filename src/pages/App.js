8dad41ceb24e329df713035d35d5edcce89be290
lagataricamarie <ricamarielagata@thelewiscollege.edu.ph>
2024-05-08 23:22:08 +0800

    finalproj

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,38 +2,45 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Sidebar from './Sidebar';
-import Students from './Students';
 import Gradebook from './Gradebook';
-import Settings from './Settings';
-import Logout from './Logout';
 import Home from './Home';
+import Course from './Course1';
+import Course2 from './Course2';
+import Course3 from './Course3';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home', 'students', 'gradebook','report', 'settings', 'logout');
-    const [toggle, setToggle] = useState(true);   
+    const [currentPage, setCurrentPage] = useState('home', 'gradebook', 'course1', 'course2'); // Set initial state to 'home'
+    const [toggle, setToggle] = useState(true);
 
     const handlePageChange = (page) => {
       setCurrentPage(page);
+      if (window.innerWidth < 768 && toggle) {
+          setToggle(false);
+      }
   };
 
     const Toggle = () => {
         setToggle(!toggle);
     };
 
+    const handleBack = () => {
+      setCurrentPage('gradebook'); // Assuming 'gradebook' is the page key for Gradebook
+  };
+
     return (
         <div className='container-fluid bg-light min-vh-100'>
             <div className='row'>
                 {toggle && (
-                    <div className='col-4 col-md-2  bg-white vh-100'>
+                    <div className='col-3 col-md-2 bg-white min-vh-100'>
                         <Sidebar setPage={handlePageChange} />
                     </div>
                 )}
-                <div className={`col ${toggle ? 'col-md-8 col-lg-10' : 'col-md-12'}`}>
-                  {currentPage === 'home' && <Home Toggle={Toggle} />}
-                  {currentPage === 'students' && <Students Toggle={Toggle} />}
-                  {currentPage === 'gradebook' && <Gradebook Toggle={Toggle} />}
-                  {currentPage === 'settings' && <Settings Toggle={Toggle} />}
-                  {currentPage === 'logout' && <Logout Toggle={Toggle} />}
+                <div className={`col ${toggle ? 'col-md-8 col-lg-10 ' : 'col-md-12'}`}>
+                    {currentPage === 'home' && <Home Toggle={Toggle} />}
+                    {currentPage === 'gradebook' && <Gradebook Toggle={Toggle} setPage={handlePageChange} />}
+                    {currentPage === 'course1' && <Course Toggle={Toggle} handleBack={handleBack}/>}
+                    {currentPage === 'course2' && <Course2 Toggle={Toggle} handleBack={handleBack}/>}
+                    {currentPage === 'course3' && <Course3 Toggle={Toggle} handleBack={handleBack}/>}
                 </div>
             </div>
         </div>
--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,35 +1,50 @@
-import React from 'react'
-import { useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import React, { useEffect, useState, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-function Nav({Toggle}) {
-    
+function Nav({ Toggle }) {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
     useEffect(() => {
         import('bootstrap/js/dist/dropdown');
         import('bootstrap/js/dist/collapse');
+
+        
+        const handleClick = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsDropdownOpen(false);
+            }
+        };
+
+        window.addEventListener('click', handleClick);
+
+        return () => {
+            window.removeEventListener('click', handleClick);
+        };
     }, []);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
     return (
-    <nav className="navbar navbar-expand-sm navbar-black bg-transparent">
-        <i className="navbar-brand bi bi-justify-left fs-4" onClick={Toggle}></i>
-        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <i className='bi bi-justify'></i>
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-            <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                <li className="nav-item dropdown">
-                    <a className="bi bi-person fs-5 me-3 nav-link dropdown-toggle text-black" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="dropdownId">
-                        <a className="dropdown-item" href="#">Profile</a>
-                        <a className="dropdown-item" href="#">Setting</a>
-                        <a className="dropdown-item" href="#">Logout</a>
+        <nav className="navbar navbar-expand-sm navbar-black bg-transparent">
+            <i className="bi bi-justify-left fs-4" onClick={Toggle}></i>
+            <ul className="navbar-nav ms-auto flex-row">
+                <li className="nav-item dropdown" ref={dropdownRef}>
+                    <button className="nav-link" onClick={toggleDropdown}>
+                        <img src="/images/OIP.jpg" alt="User" style={{ width: '1.9em', height: '1.9em' }} />
+                    </button>
+                    <div className={`dropdown-menu dropdown-menu-end ${isDropdownOpen ? 'show' : ''}`} style={{ position: 'absolute', top: '100%', right: 0 }}>
+                        <button className="dropdown-item" onClick={() => console.log('Profile clicked')}>Profile</button>
+                        <button className="dropdown-item" onClick={() => console.log('Settings clicked')}>Settings</button>
+                        <button className="dropdown-item" onClick={() => console.log('Logout clicked')}>Logout</button>
                     </div>
                 </li>
             </ul>
-        </div>
-    </nav>
-    )
+        </nav>
+    );
 }
+
 export default Nav;
